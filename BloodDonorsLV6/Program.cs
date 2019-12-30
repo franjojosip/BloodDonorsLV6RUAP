25755d85416e41cf433e3304ef72e2b953336beb
franjojosip <franjojosip.jukic2@gmail.com>
2019-12-30 18:46:05 +0100

    Create C# application

---
+++ BloodDonorsLV6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BloodDonorsLV6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
